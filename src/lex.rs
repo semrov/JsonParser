8945e3289e9b5e9c53759c38a218adbc43e79ba6
Jure <jure.semrov@outlook.com>
2018-04-15 18:32:49 +0200

    Read UTF-8 string value

--- src/lex.rs
+++ src/lex.rs
@@ -1,3 +1,4 @@
+use std::{char, str};
 
 /// A JSON lexical analyzer of UTF-8 string
 /// 
@@ -58,15 +59,82 @@ impl<'src> Lex<'src> {
         // Determine the type of the token by its first byte
         let (token_type, rest) = match *self.source
         {
+            //read structural characters
             [b'{', ref rest..] => (TokenType::LeftBrace, rest),
             [b'}', ref rest..] => (TokenType::RightBrace, rest),
             [b'[', ref rest..] => (TokenType::LeftBracket, rest),
             [b']', ref rest..] => (TokenType::RightBracket, rest),
             [b',', ref rest..] => (TokenType::Comma, rest),
             [b':', ref rest..] => (TokenType::Colon, rest),
-            
-        }
 
+            //read string
+            [b'"', ref rest..] => Self::read_string(rest),
+            
+        };
+
+    }
+
+    fn read_string(mut source : &'src [u8]) -> (TokenType,&'src [u8])
+    {
+        let mut buffer = String::new();
+        loop
+        {
+            match *source
+            {
+                //closing quote
+                [b'"', ref rest..] => {source = rest; break;},
+
+                //escape sequences
+                [b'\\', b'"', ref rest..] => {source = rest; buffer.push_str("\"");},
+                [b'\\', b'\\', ref rest..] => {source = rest; buffer.push_str("\\");},
+                [b'\\', b'/', ref rest..] => {source = rest; buffer.push_str("/");},
+                [b'\\', b'b', ref rest..] => {source = rest; buffer.push_str("\x08");},
+                [b'\\', b'f', ref rest..] => {source = rest; buffer.push_str("\x0c");},
+                [b'\\', b'n', ref rest..] => {source = rest; buffer.push_str("\n");},
+                [b'\\', b'r', ref rest..] => {source = rest; buffer.push_str("\r");},
+                [b'\\', b't', ref rest..] => {source = rest; buffer.push_str("\t");},
+                [b'\\', b'u', ref rest..] => 
+                {
+                    //add function reading unicode escape
+
+                    source = rest; 
+                    buffer.push_str("");
+                },
+
+                //UTF8 codepoints
+                [0x00..=0x7F, ref rest..] => 
+                {
+                    source = rest;
+                    let s = unsafe {str::from_utf8_unchecked(source.get_unchecked(..1))};
+                    buffer.push_str(s);
+                },
+                [0xC0..=0xDF,0x80..=0xBF, ref rest..] => 
+                {
+                    source = rest;
+                    let s = unsafe {str::from_utf8_unchecked(source.get_unchecked(..2))};
+                    buffer.push_str(s);
+                },
+                [0xE0..=0xEF, 0x80..=0xBF,0x80..=0xBF, ref rest..] =>
+                {
+                    source = rest;
+                    let s = unsafe {str::from_utf8_unchecked(source.get_unchecked(..3))};
+                    buffer.push_str(s);
+                },
+                [0xF0..=0xF7,0x80..=0xBF,0x80..=0xBF,0x80..=0xBF, ref rest..] => 
+                {
+                    source = rest;
+                    let s = unsafe {str::from_utf8_unchecked(source.get_unchecked(..4))};
+                    buffer.push_str(s);
+                },
+
+                //If string is valid utf8 string, then this should not happen
+                [_,_..] => unreachable!(),
+
+                //unterminated string
+                 [ref rest..] => return (TokenType::Error,rest),
+            }
+        }
+        (TokenType::String(buffer),source)
     }
 }
 
